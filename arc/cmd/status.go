747a8d97abf646128ea4d0f65804297f89830924
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2022-07-30 05:08:07 +0000

    Allow a CircleCI employee mode

--- arc/cmd/status.go
+++ arc/cmd/status.go
@@ -25,63 +25,72 @@ type spResponse struct {
 	Status *spStatus `json:"status"`
 }
 
-var statusCmd = &cobra.Command{
-	Use:   "status",
-	Short: "Gives you the status page status of both CircleCI and GitHub",
-	Run: func(cmd *cobra.Command, args []string) {
+var (
+	cciFl bool
 
-		var cciResp *spResponse
-		var ghResp *spResponse
-		var linodeResp *spResponse
-		cciURL := "https://status.circleci.com/api/v2/status.json"
-		ghURL := "https://www.githubstatus.com/api/v2/status.json"
-		linodeURL := "https://status.linode.com/api/v2/status.json"
+	statusCmd = &cobra.Command{
+		Use:   "status",
+		Short: "Gives you the status page status of both CircleCI and GitHub",
+		Run: func(cmd *cobra.Command, args []string) {
 
-		client := New()
+			var cciResp *spResponse
+			var ghResp *spResponse
+			var linodeResp *spResponse
+			cciURL := "https://status.circleci.com/api/v2/status.json"
+			ghURL := "https://www.githubstatus.com/api/v2/status.json"
+			linodeURL := "https://status.linode.com/api/v2/status.json"
 
-		errCCI := client.getJSON(cciURL, &cciResp)
-		errGH := client.getJSON(ghURL, &ghResp)
-		errLinode := client.getJSON(linodeURL, &linodeResp)
+			client := New()
 
-		if errCCI != nil {
-			cciResp.Status.Indicator = "can't connect"
-		}
+			errCCI := client.getJSON(cciURL, &cciResp)
+			errGH := client.getJSON(ghURL, &ghResp)
+			errLinode := client.getJSON(linodeURL, &linodeResp)
 
-		if errGH != nil {
-			ghResp.Status.Indicator = "can't connect"
-		}
+			if errCCI != nil {
+				cciResp.Status.Indicator = "can't connect"
+			}
 
-		if errLinode != nil {
-			linodeResp.Status.Indicator = "can't connect"
-		}
+			if errGH != nil {
+				ghResp.Status.Indicator = "can't connect"
+			}
 
-		var cciTabs = ""
-		if cciResp.Status.Indicator == "none" {
-			cciResp.Status.Indicator = ""
-			cciTabs = "\t"
-		}
+			if errLinode != nil {
+				linodeResp.Status.Indicator = "can't connect"
+			}
 
-		var ghTabs = ""
-		if ghResp.Status.Indicator == "none" {
-			ghResp.Status.Indicator = ""
-			ghTabs = "\t"
-		}
+			var cciTabs = ""
+			if cciResp.Status.Indicator == "none" {
+				cciResp.Status.Indicator = ""
+				cciTabs = "\t"
+			}
 
-		var linodeTabs = ""
-		if linodeResp.Status.Indicator == "none" {
-			linodeResp.Status.Indicator = ""
-			linodeTabs = "\t"
-		}
+			var ghTabs = ""
+			if ghResp.Status.Indicator == "none" {
+				ghResp.Status.Indicator = ""
+				ghTabs = "\t"
+			}
 
-		fmt.Println("Reporting status page results...")
-		fmt.Println("")
-		fmt.Printf("CircleCI:\t%s%s\t%s\n", cciResp.Status.Indicator, cciTabs, cciResp.Status.Description)
-		fmt.Printf("GitHub:\t\t%s%s\t%s\n", ghResp.Status.Indicator, ghTabs, ghResp.Status.Description)
-		fmt.Printf("Linode:\t\t%s%s\t%s\n", linodeResp.Status.Indicator, linodeTabs, linodeResp.Status.Description)
-	},
-}
+			var linodeTabs = ""
+			if linodeResp.Status.Indicator == "none" {
+				linodeResp.Status.Indicator = ""
+				linodeTabs = "\t"
+			}
+
+			fmt.Println("Reporting status page results...")
+			fmt.Println("")
+			fmt.Printf("CircleCI:\t%s%s\t%s\n", cciResp.Status.Indicator, cciTabs, cciResp.Status.Description)
+			fmt.Printf("GitHub:\t\t%s%s\t%s\n", ghResp.Status.Indicator, ghTabs, ghResp.Status.Description)
+			if !cciFl {
+				fmt.Printf("Linode:\t\t%s%s\t%s\n", linodeResp.Status.Indicator, linodeTabs, linodeResp.Status.Description)
+			}
+		},
+	}
+)
 
 func init() {
 
+	// Temporary flag to hide non-CircleCI related statuses
+	statusCmd.Flags().BoolVar(&cciFl, "cci", false, "only show CircleCI related statuses")
+	statusCmd.Flags().MarkHidden("cci")
 	rootCmd.AddCommand(statusCmd)
 }
