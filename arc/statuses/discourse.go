08857f6de08a635450d6ea714dceaf9cfb71aa2a
Ricardo N Feliciano <Ricardo@Feliciano.Tech>
2024-09-20 02:12:33 -0400

    status: Add support for Discourse

---
+++ arc/statuses/discourse.go
@@ -0,0 +1,8 @@
+package statuses
+
+/*
+ * Register the Docker (company) status page.
+ */
+func init() {
+	RegisterStatusIOPage("discourse", "https://status.discourse.org/1.0/status/5e2141ce30dc5c04b3ac32fc")
+}
